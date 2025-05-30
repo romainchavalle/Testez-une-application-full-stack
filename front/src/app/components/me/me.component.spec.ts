0df5fef3beae26541633edf3eaace29e4f434b98
romain chavalle <romain.chavalle@gmail.com>
2025-05-30 11:28:32 +0200

    testing me component

--- front/src/app/components/me/me.component.spec.ts
+++ front/src/app/components/me/me.component.spec.ts
@@ -8,6 +8,9 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { SessionService } from 'src/app/services/session.service';
 
 import { MeComponent } from './me.component';
+import { of } from 'rxjs';
+import { UserService } from 'src/app/services/user.service';
+import { By } from '@angular/platform-browser';
 
 describe('MeComponent', () => {
   let component: MeComponent;
@@ -19,6 +22,23 @@ describe('MeComponent', () => {
       id: 1
     }
   }
+
+  const mockUser = {
+    id: 1,
+    email: 'test@example.com',
+    firstName: 'John',
+    lastName: 'Doe',
+    admin: true,
+    password: '',
+    createdAt: 'January 1, 2020',
+    updatedAt: 'January 1, 2021'
+  };
+
+  const mockUserService = {
+    getById: jest.fn().mockReturnValue(of(mockUser)),
+  }
+
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [MeComponent],
@@ -30,7 +50,10 @@ describe('MeComponent', () => {
         MatIconModule,
         MatInputModule
       ],
-      providers: [{ provide: SessionService, useValue: mockSessionService }],
+      providers: [
+        { provide: SessionService, useValue: mockSessionService},
+        { provide: UserService, useValue: mockUserService}
+      ],
     })
       .compileComponents();
 
@@ -46,7 +69,21 @@ describe('MeComponent', () => {
 
   it("should display user's informations", () => {
     // Test to do
-    
+
+    const nameEl = fixture.debugElement.query(By.css('p:nth-of-type(1)')).nativeElement;
+    const emailEl = fixture.debugElement.query(By.css('p:nth-of-type(2)')).nativeElement;
+    const adminEl = fixture.debugElement.query(By.css('p.my2'));
+    const createdAtEl = fixture.debugElement.queryAll(By.css('.p2 p'))[0].nativeElement;
+    const updatedAtEl = fixture.debugElement.queryAll(By.css('.p2 p'))[1].nativeElement;
+
+    expect(nameEl.textContent).toContain(mockUser.firstName);
+    expect(nameEl.textContent).toContain(mockUser.lastName.toUpperCase());
+    expect(emailEl.textContent).toContain(mockUser.email);
+
+    expect(adminEl.nativeElement.textContent).toContain("You are admin");
+
+    expect(createdAtEl.textContent).toContain(mockUser.createdAt);
+    expect(updatedAtEl.textContent).toContain(mockUser.updatedAt);
 
   });
 
