87c161cce16a3988f11d9ef744f1c31b36dcad51
romain chavalle <romain.chavalle@gmail.com>
2025-06-09 11:04:30 +0200

    finish E2E testing edit action

--- front/cypress/e2e/sessions/update.cy.ts
+++ front/cypress/e2e/sessions/update.cy.ts
@@ -1,8 +1,10 @@
 describe('form spec edit session', () => {
   it('should edit session after complete the form', () => {
 
-        // Mock the sessions data
-    const mockSessions = [
+    let shouldReturnUpdatedData = false;
+
+    // Mock the sessions data
+    const originalSessions = [
       {
         id: 1,
         name: 'Yoga du matin',
@@ -17,15 +19,30 @@ describe('form spec edit session', () => {
       }
     ];
 
+    const updatedSessions = [
+      {
+        id: 1,
+        name: 'testnamemodified',
+        date: '2025-06-04T10:00:00Z',
+        description: 'Séance de yoga relaxante'
+      },
+      {
+        id: 2,
+        name: 'Pilates',
+        date: '2025-06-05T17:00:00Z',
+        description: 'Travail du centre du corps'
+      }
+    ];
 
-    cy.intercept('GET', '**/api/session', {
-      statusCode: 200,
-      body: mockSessions
+    cy.intercept('GET', '**/api/session', (req) => {
+      req.reply({
+        statusCode: 200,
+        body: shouldReturnUpdatedData ? updatedSessions : originalSessions
+      });
     }).as('getSessions');
 
+
     // MOCK the teacher data
-
-
     const mockTeachers = [
     {
       id: 1,
@@ -49,6 +66,9 @@ describe('form spec edit session', () => {
     }).as('getTeachers');
 
 
+    // Mock the update request
+    cy.intercept('PUT', '**/api/session/*').as('updateSession');
+
     // Log in
     cy.login();
 
@@ -61,17 +81,29 @@ describe('form spec edit session', () => {
     // Vérifier page detail
     cy.url().should('include', '/update');
 
-
     // Attendre que la requête mockée soit terminée
     cy.wait('@getTeachers');
 
     // modifier valeur du name
     cy.get('input[formControlName="name"]').clear().type('testnamemodified');
 
+    // Submit le form
     cy.get('button[type="submit"]').click();
+
+
+    // Verifier que la requete contient bien le nouveau name modifié
+    cy.wait('@updateSession').its('request.body.name').should('eq', 'testnamemodified');
+
+    // Mock the updated sessions data
+    cy.then(() => {
+      shouldReturnUpdatedData = true;
+    });
+
+    cy.wait('@getSessions');
 
     cy.url().should('include', '/sessions');
 
+    // Verifier que le nouveau name apparait dans le DOM
     cy.contains('testnamemodified').should('exist');
 
   });
